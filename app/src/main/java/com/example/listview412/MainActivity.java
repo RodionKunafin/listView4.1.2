b52a159d3f429b3bf959e6d43a002c74872ddbed
Rodion <george.t.o.p@mail.ru>
2020-04-01 20:51:05 +0300

    Работает)))

--- app/src/main/java/com/example/listview412/MainActivity.java
+++ app/src/main/java/com/example/listview412/MainActivity.java
@@ -5,16 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
